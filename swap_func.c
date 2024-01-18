0810d9754b672b720d5d6167bacb6fefa0085b7b
Kamal <malikleet1337@gmail.com>
2024-01-18 17:48:33 +0100

    The swap function swaps the two top elements of the stack

---
+++ swap_func.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * swap_func - Swaps the top two elements of the stack.
+ *
+ * Description: This function swaps the positions of the top two elements
+ * of the stack. It checks if the stack has at least two elements; if not,
+ * it prints an error message and exits with failure status. Otherwise,
+ * it swaps the top two elements by adjusting the pointers accordingly.
+ *
+ * @stack: Pointer to the pointer to the top of the stack.
+ * @line_number: The line number where the "swap" operation appears
+ * in the Monty file.
+ */
+
+void swap_func(stack_t **stack, unsigned int line_number)
+{
+	int len = stack_len(stack);
+	stack_t *tmp = NULL;
+
+	tmp = *stack;
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		free_memory();
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *stack;
+	*stack = (*stack)->next;
+
+	tmp->next = (*stack)->next;
+	tmp->prev = *stack;
+
+	(*stack)->next = tmp;
+	(*stack)->prev = NULL;
+}
